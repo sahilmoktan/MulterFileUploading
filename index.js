92937aa7ea6a0ad986c183760d5d4aae2200e3c2
Sahil Moktan <moktanshail9@gmail.com>
2023-06-12 00:16:06 +0545

    ejs module added, single file upload successfully but file corrupted

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const path = require("path");
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
 const PORT = 8000;
 
+const upload = multer({ dest: "uploads/" });
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   return res.render("homepage");
+});
+
+app.post("/upload", upload.single("profileImage"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+
+  return res.redirect("/");
 });
 
 app.listen(PORT, () => console.log(`SERVER STARTED AT ${PORT}`));
