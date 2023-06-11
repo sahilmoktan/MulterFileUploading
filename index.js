85ceb84eabd9dc572057182303b6c2d5122fe85d
Sahil Moktan <moktanshail9@gmail.com>
2023-06-11 23:57:04 +0545

    basic boiller plate establish for multer understanding

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.render("homepage");
+});
+
+app.listen(PORT, () => console.log(`SERVER STARTED AT ${PORT}`));
